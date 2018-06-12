39882a4bac7b7117f726c866d44c6d048ce6c22a
lracca1 <lracca@cnm.edu>
2018-06-12 00:53:58 -0600

    Added Test class.

---
+++ Test/edu/cnm/deepdive/cards/SuitTest.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.cards;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SuitTest {
+
+  @Test
+  void testToString() {
+  }
+}
